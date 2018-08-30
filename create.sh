7c6993b54f36817ddb0a8c3fc220793aaec56b86
David McClure <davidwilliammcclure@gmail.com>
2018-08-30 15:35:08 -0400

    Add config/{ansible,terraform}

--- create.sh
+++ create.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-(cd terraform && terraform apply)
-(cd ansible && pipenv run ansible-playbook deploy.yml)
+(cd terraform && ln -s ../config/*.auto.tfvars .)
+
+# (cd terraform && terraform apply)
+# (cd ansible && pipenv run ansible-playbook deploy.yml)
