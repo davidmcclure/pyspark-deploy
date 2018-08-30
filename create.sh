eb94cacc3dcd5a37ad7b78a109fe664d7b3185a0
David McClure <davidwilliammcclure@gmail.com>
2018-08-30 15:50:47 -0400

    Symlink configs into Ansible/Terraform.

--- create.sh
+++ create.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-(cd terraform && ln -s ../config/*.auto.tfvars .)
+ln -sf $PWD/config/terraform/*.auto.tfvars $PWD/terraform
+ln -sf $PWD/config/ansible/*.yml $PWD/ansible/group_vars/all
 
-# (cd terraform && terraform apply)
-# (cd ansible && pipenv run ansible-playbook deploy.yml)
+(cd terraform && terraform apply)
+(cd ansible && pipenv run ansible-playbook deploy.yml)
