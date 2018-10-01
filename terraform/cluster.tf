4c6b8c7cb0f2badc0d294fe6da1be22e3283040e
David McClure <davidwilliammcclure@gmail.com>
2018-10-01 16:59:24 -0400

    Move the Spark security group definition into cluster.tf.

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -1,8 +1,84 @@
+resource "aws_security_group" "spark" {
+  name        = "${var.name}-spark"
+  description = "Standalone Spark cluster"
+  vpc_id      = "${aws_vpc.spark.id}"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+  }
+
+  egress {
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+  }
+
+  egress {
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+  }
+
+  # All ports open intra-group, for Spark random assignment.
+  ingress {
+    from_port = 0
+    to_port   = 65535
+    self      = true
+    protocol  = "tcp"
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 65535
+    self      = true
+    protocol  = "tcp"
+  }
+
+  # Spark UI
+  ingress {
+    from_port   = 8080
+    to_port     = 8081
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+  }
+
+  # Spark UI "Application detail"
+  ingress {
+    from_port   = 4040
+    to_port     = 4040
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+  }
+
+  tags {
+    Name = "${var.name}-spark"
+  }
+}
+
 resource "aws_instance" "master" {
   ami                         = "${var.base_ami}"
   instance_type               = "${var.master_instance_type}"
   subnet_id                   = "${aws_subnet.spark.id}"
-  vpc_security_group_ids      = ["${aws_security_group.spark-spark.id}"]
+  vpc_security_group_ids      = ["${aws_security_group.spark.id}"]
   key_name                    = "${aws_key_pair.spark.id}"
   associate_public_ip_address = true
 
@@ -15,7 +91,7 @@ resource "aws_instance" "worker" {
   ami                         = "${var.base_ami}"
   instance_type               = "${var.worker_instance_type}"
   subnet_id                   = "${aws_subnet.spark.id}"
-  vpc_security_group_ids      = ["${aws_security_group.spark-spark.id}"]
+  vpc_security_group_ids      = ["${aws_security_group.spark.id}"]
   key_name                    = "${aws_key_pair.spark.id}"
   associate_public_ip_address = true
 
