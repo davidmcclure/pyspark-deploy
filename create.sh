db8a2117a699d7f7b17479edf777ee07af73b372
David McClure <davidwilliammcclure@gmail.com>
2018-10-01 16:45:08 -0400

    Un-nest /config.

--- create.sh
+++ create.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-ln -sf $PWD/config/terraform/*.auto.tfvars $PWD/terraform
-ln -sf $PWD/config/ansible/*.yml $PWD/ansible/group_vars/all
+ln -sf $PWD/config/*.auto.tfvars $PWD/terraform
+ln -sf $PWD/config/*.yml $PWD/ansible/group_vars/all
 
 (cd terraform && terraform apply)
 (cd ansible && pipenv run ansible-playbook deploy.yml)
