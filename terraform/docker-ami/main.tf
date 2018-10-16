87e6dc5e5c4ec6b735e999c7a9bd6f0d78d018ab
David McClure <davidwilliammcclure@gmail.com>
2018-10-16 12:43:20 -0400

    Stub in separate docker-ami role + playbook.

---
+++ terraform/docker-ami/main.tf
@@ -0,0 +1,57 @@
+module "vpc" {
+  source = "../modules/vpc"
+}
+
+provider "aws" {
+  region = "${module.vpc.aws_region}"
+}
+
+resource "aws_security_group" "docker" {
+  name        = "${var.name}"
+  description = "Docker AMI"
+  vpc_id      = "${module.vpc.vpc_id}"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+  }
+
+  tags {
+    Name = "${var.name}"
+  }
+}
+
+resource "aws_instance" "docker" {
+  ami                         = "${var.base_ami}"
+  instance_type               = "${var.instance_type}"
+  subnet_id                   = "${module.vpc.subnet_id}"
+  vpc_security_group_ids      = ["${aws_security_group.docker.id}"]
+  key_name                    = "${module.vpc.key_name}"
+  associate_public_ip_address = true
+
+  tags {
+    Name = "${var.name}"
+  }
+}
+
+data "template_file" "inventory" {
+  template = "${file("${path.module}/inventory.tpl")}"
+
+  vars {
+    docker_ip = "${aws_instance.docker.public_ip}"
+  }
+}
+
+resource "local_file" "inventory" {
+  content  = "${data.template_file.inventory.rendered}"
+  filename = "${path.module}/../.inventory/docker-ami"
+}
---
+++ terraform/docker-ami/variables.tf
@@ -0,0 +1,11 @@
+variable "name" {
+  default = "docker"
+}
+
+variable "base_ami" {
+  default = "ami-0ac019f4fcb7cb7e6"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
