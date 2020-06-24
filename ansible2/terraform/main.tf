4d33662194cecae55f02df70b7045bb9dd6e9be6
David McClure <davidwilliammcclure@gmail.com>
2020-06-24 14:13:43 -0400

    Start to block in unified Ansible (wrapping TF) approach.

---
+++ ansible2/terraform/main.tf
@@ -0,0 +1,146 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_security_group" "spark" {
+
+  description = "Standalone Spark cluster"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+  }
+
+  egress {
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+  }
+
+  egress {
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+  }
+
+  # All ports open intra-group, for Spark random assignment.
+  ingress {
+    from_port = 0
+    to_port   = 65535
+    self      = true
+    protocol  = "tcp"
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 65535
+    self      = true
+    protocol  = "tcp"
+  }
+
+  # Spark UI
+  ingress {
+    from_port   = 8080
+    to_port     = 8081
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+  }
+
+  # Spark UI "Application detail"
+  ingress {
+    from_port   = 4040
+    to_port     = 4040
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+  }
+}
+
+resource "aws_key_pair" "spark" {
+  public_key = file(var.public_key_path)
+}
+
+resource "aws_instance" "master" {
+  ami                         = var.docker_ami
+  instance_type               = var.master_instance_type
+  subnet_id                   = var.subnet_id
+  vpc_security_group_ids      = [aws_security_group.spark.id]
+  key_name                    = aws_key_pair.spark.key_name
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "spark-master"
+  }
+
+  root_block_device {
+    volume_size = var.master_root_vol_size
+  }
+}
+
+resource "aws_spot_instance_request" "worker" {
+  ami                         = var.docker_ami
+  instance_type               = var.worker_instance_type
+  subnet_id                   = var.subnet_id
+  vpc_security_group_ids      = [aws_security_group.spark.id]
+  key_name                    = aws_key_pair.spark.key_name
+  spot_price                  = var.spot_price
+  spot_type                   = var.spot_type
+  associate_public_ip_address = true
+  wait_for_fulfillment        = true
+  count                       = var.worker_count
+
+  root_block_device {
+    volume_size = var.worker_root_vol_size
+  }
+}
+
+# locals {
+#   # Worker IP list for template.
+#   worker_ips_str = join("\n", [
+#     for ip in aws_spot_instance_request.worker.*.public_ip :
+#     ip if ip != null
+#   ])
+# }
+
+# data "template_file" "inventory" {
+#   template = file("${path.module}/inventory.tpl")
+
+#   vars = {
+#     master_ip              = aws_instance.master.public_ip
+#     worker_ips             = local.worker_ips_str
+#     master_private_ip      = aws_instance.master.private_ip
+#     driver_memory          = var.driver_memory
+#     driver_max_result_size = var.driver_max_result_size
+#     executor_memory        = var.executor_memory
+#     master_docker_runtime  = var.master_docker_runtime
+#     worker_docker_runtime  = var.worker_docker_runtime
+#   }
+# }
+
+# resource "local_file" "inventory" {
+#   content  = data.template_file.inventory.rendered
+#   filename = "${path.module}/.inventory"
+# }
+
+# resource "local_file" "master_ip" {
+#   content  = aws_instance.master.public_ip
+#   filename = "${path.module}/.master-ip"
+# }
