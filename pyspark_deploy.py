3bbaea42c92e4580f09f516c51aac21bd7c64e33
David McClure <davidwilliammcclure@gmail.com>
2022-05-01 22:04:25 -0400

    ClusterState vs Cluster

--- pyspark_deploy.py
+++ pyspark_deploy.py
@@ -3,8 +3,10 @@ import json
 import smart_open
 import time
 import requests
+import addict
 
 from datetime import datetime
+from pathlib import Path
 from typing import Callable, Optional
 from dataclasses import dataclass
 from rich.console import Console
@@ -16,83 +18,148 @@ console = Console()
 def wait_for(check: Callable, interval: int = 3):
     t1 = datetime.now()
     while True:
-        try:
-            check()
+        if check():
             return
-        except:
+        else:
             yield datetime.now() - t1
             time.sleep(interval)
 
 
-# TODO: state_path
+@dataclass
+class ClusterState:
+
+    state_path: str = 'terraform.tfstate'
+
+    # TODO: Pass config
+    def create(self):
+        with console.status('Creating cluster...'):
+            subprocess.run(
+                ['terraform', 'apply', '-auto-approve', '-state', self.state_path],
+                capture_output=True,
+            )
+
+        cluster = self.get_cluster()
+
+        # TODO: Log the elapsed time.
+        with console.status('Waiting for API...'):
+            for elapsed in wait_for(lambda: cluster.ready):
+                pass
+
+        return cluster
+
+    def destroy(self):
+        with console.status('Destroying cluster...'):
+            subprocess.run(
+                ['terraform', 'destroy', '-auto-approve', '-state', self.state_path],
+                capture_output=True,
+            )
+
+    def read_tfstate(self) -> Optional[dict]:
+        path = Path(self.state_path)
+        if path.exists():
+            return json.load(path.open())
+
+    def read_master_dns(self) -> Optional[str]:
+        if state := self.read_tfstate():
+            return addict.Dict(state).outputs.master_dns.value or None
+
+    def get_cluster(self):
+        if master_dns := self.read_master_dns():
+            return Cluster(master_dns)
+        else:
+            raise Exception('Cluster is down. Call create().')
+
+
+@dataclass
 class Cluster:
 
-    def ping_api(self) -> bool:
-        """Ping the job submission API. When this is online, the cluster is up
-        and ready for submissions.
-        """
+    master_dns: str
+
+    @property
+    def api_url(self) -> str:
+        return f'http://{self.master_dns}:6066'
+
+    def ping(self) -> bool:
         try:
-            requests.get(f'http://{self.master_dns}:6066')
+            requests.get(self.api_url)
             return True
         except:
             return False
 
     @property
     def ready(self):
-        return self.ping_api()
+        return self.ping()
+
+
+# TODO: state_path
+# class Cluster:
+
+#     def ping_api(self) -> bool:
+#         """Ping the job submission API. When this is online, the cluster is up
+#         and ready for submissions.
+#         """
+#         try:
+#             requests.get(f'http://{self.master_dns}:6066')
+#             return True
+#         except:
+#             return False
+
+#     @property
+#     def ready(self):
+#         return self.ping_api()
     
-    # TODO: config object
-    def create(self):
-        """Create a cluster with a given configuration. 
-        """
-        with console.status('Creating cluster...'):
-            subprocess.run(
-                ['terraform', 'apply', '-auto-approve'],
-                capture_output=True,
-            )
+#     # TODO: config object
+#     def create(self):
+#         """Create a cluster with a given configuration. 
+#         """
+#         with console.status('Creating cluster...'):
+#             subprocess.run(
+#                 ['terraform', 'apply', '-auto-approve'],
+#                 capture_output=True,
+#             )
 
-        with console.status('Waiting for API...'):
-            for elapsed in wait_for(self.ping_api):
-                print(elapsed)
+#         with console.status('Waiting for API...'):
+#             for elapsed in wait_for(self.ping_api):
+#                 print(elapsed)
 
-    def destroy(self):
-        with console.status('Destroying cluster...'):
-            subprocess.run(
-                ['terraform', 'destroy', '-auto-approve'],
-                capture_output=True,
-            )
+#     def destroy(self):
+#         with console.status('Destroying cluster...'):
+#             subprocess.run(
+#                 ['terraform', 'destroy', '-auto-approve'],
+#                 capture_output=True,
+#             )
 
-    # TODO: Does this work with s3 state / does it matter?
-    def read_tfstate(self) -> dict:
-        with smart_open.open('terraform.tfstate') as fh:
-            return json.load(fh)
+#     # TODO: Does this work with s3 state / does it matter?
+#     def read_tfstate(self) -> dict:
+#         with smart_open.open('terraform.tfstate') as fh:
+#             return json.load(fh)
 
-    # TODO: Raise exception if absent.
-    @property
-    def master_dns(self):
-        state = self.read_tfstate()
-        if master_dns := state['outputs'].get('master_dns'):
-            return master_dns['value']
+#     # TODO: Raise exception if absent.
+#     @property
+#     def master_dns(self):
+#         state = self.read_tfstate()
+#         if master_dns := state['outputs'].get('master_dns'):
+#             return master_dns['value']
 
-    def submit(
-        self,
-        path: str,
-        app_args: Optional[list[str]] = None,
-        spark_properties: Optional[dict] = None,
-    ):
-        return requests.post(f'http://{self.master_dns}:6066/v1/submissions/create', json={
-            "appResource": f"file:{path}",
-            "appArgs": [path],
-            "sparkProperties": {
-                "spark.app.name": "os-corpus"
-            },
-            "clientSparkVersion": "3.2.1",
-            "mainClass": "org.apache.spark.deploy.SparkSubmit",
-            "environmentVariables": {
-                "SPARK_ENV_LOADED": "1"
-            },
-            "action": "CreateSubmissionRequest"
-        })
+#     def submit(
+#         self,
+#         path: str,
+#         app_args: Optional[list[str]] = None,
+#         spark_properties: Optional[dict] = None,
+#     ):
+#         return requests.post(f'http://{self.master_dns}:6066/v1/submissions/create', json={
+#             "appResource": f"file:{path}",
+#             "appArgs": [path],
+#             "sparkProperties": {
+#                 "spark.app.name": "os-corpus"
+#             },
+#             "clientSparkVersion": "3.2.1",
+#             "mainClass": "org.apache.spark.deploy.SparkSubmit",
+#             "environmentVariables": {
+#                 "SPARK_ENV_LOADED": "1"
+#             },
+#             "action": "CreateSubmissionRequest"
+#         })
 
 
 # cluster = Cluster()
