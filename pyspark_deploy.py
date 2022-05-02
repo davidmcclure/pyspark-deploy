87768e30f816c4320d35eab22ba8d7366a5b0fde
David McClure <davidwilliammcclure@gmail.com>
2022-05-02 00:57:21 -0400

    ClusterConfig

--- pyspark_deploy.py
+++ pyspark_deploy.py
@@ -11,14 +11,37 @@ from pathlib import Path
 from typing import Callable, Optional
 from dataclasses import dataclass
 from rich.console import Console
-
-"""
-args / spark props / env to submit()
-cluster configs
-"""
+from pydantic import BaseModel
 
 
 console = Console()
+
+
+class ClusterConfig(BaseModel):
+    ecr_server: str
+    ecr_repo: str
+    aws_vpc_id: str
+    aws_subnet_id: str
+    aws_access_key_id: str
+    aws_secret_access_key: str
+    wandb_api_key: str
+    aws_region: str = 'us-east-1'
+    aws_ami: str = 'ami-04eb5b2f5ef92e8b8'
+    public_key_path: str = '~/.ssh/spark.pub'
+    root_vol_size: int = 100
+    master_instance_type: str = 'c5.xlarge'
+    driver_memory: str = '4g'
+    worker_instance_type: str = 'c5.xlarge'
+    executor_memory: str = '4g'
+    spot_worker_count: int = 0
+    on_demand_worker_count: int = 0
+    gpu_workers: bool = False
+    data_dir: str = '/data'
+    max_driver_result_size: str = "1g"
+    max_task_failures: int = 20
+    spark_packages: list[str] = (
+        'org.apache.spark:spark-hadoop-cloud_2.13:3.2.1',
+    )
 
 
 # TODO: Just block until done, don't return a generator?
